8e8ae182e26aa98dc2c6c7386eaf855e132e853b
V.Ognyanova <103129757+VOgnyanova@users.noreply.github.com>
2023-07-19 12:09:33 +0300

    Exercise Spring Data Advanced Querying

---
+++ Ex_skeleton/src/main/java/bg/softuni/bookshop/domain/entities/BaseEntity.java
@@ -0,0 +1,22 @@
+package bg.softuni.bookshop.domain.entities;
+
+import javax.persistence.*;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ Ex_skeleton/src/main/java/bg/softuni/bookshop/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package bg.softuni.bookshop.repositories;
+
+import bg.softuni.bookshop.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
